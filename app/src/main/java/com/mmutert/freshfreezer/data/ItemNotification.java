4e057bbbce05495e66d5f31dccbb8765d2c1c86f
michaelmutert <michael.mutert@rwth-aachen.de>
2020-06-16 15:18:17 +0200

    Switch notification id column to strings for UUID

--- app/src/main/java/com/mmutert/freshfreezer/data/ItemNotification.java
+++ app/src/main/java/com/mmutert/freshfreezer/data/ItemNotification.java
@@ -4,40 +4,37 @@ import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 
 @Entity(tableName = "notifications",
         foreignKeys = @ForeignKey(entity = FrozenItem.class,
                                   parentColumns = "id",
-                                  childColumns = "itemId",
-                                  onUpdate = ForeignKey.CASCADE))
+                                  childColumns = "item_id",
+                                  onUpdate = ForeignKey.CASCADE),
+        indices = @Index(name = "ItemIdIndex", value = "item_id"))
 public class ItemNotification {
 
     @PrimaryKey()
     @NonNull
     @ColumnInfo(name = "notification_id")
-    private int notificationId;
+    private String notificationId;
 
     @ColumnInfo(name = "item_id")
     private long itemId;
 
-    public ItemNotification() {
-    }
-
-    public int getNotificationId() {
-        return notificationId;
-    }
-
-    public void setNotificationId(final int notificationId) {
+    public ItemNotification(@NonNull final String notificationId, final long itemId) {
         this.notificationId = notificationId;
+        this.itemId = itemId;
+    }
+
+    @NonNull
+    public String getNotificationId() {
+        return notificationId;
     }
 
     public long getItemId() {
         return itemId;
-    }
-
-    public void setItemId(final long itemId) {
-        this.itemId = itemId;
     }
 }
