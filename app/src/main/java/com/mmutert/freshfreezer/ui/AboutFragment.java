944b34da096dab016f5a6c7c99863831fc52b9da
michaelmutert <michael.mutert@rwth-aachen.de>
2020-07-25 18:37:01 +0200

    Add functionality to Github, Mail, and Issues buttons in About screen

--- app/src/main/java/com/mmutert/freshfreezer/ui/AboutFragment.java
+++ app/src/main/java/com/mmutert/freshfreezer/ui/AboutFragment.java
@@ -1,14 +1,22 @@
 package com.mmutert.freshfreezer.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.mmutert.freshfreezer.BuildConfig;
 import com.mmutert.freshfreezer.R;
+import com.mmutert.freshfreezer.databinding.FragmentAboutBinding;
+
+import java.net.URL;
 
 
 /**
@@ -17,6 +25,10 @@ import com.mmutert.freshfreezer.R;
  * create an instance of this fragment.
  */
 public class AboutFragment extends Fragment {
+
+
+    private FragmentAboutBinding mBinding;
+
 
     public AboutFragment() {
         // Required empty public constructor
@@ -50,6 +62,51 @@ public class AboutFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_about, container, false);
+
+        mBinding.tvAboutVersion.setText(getString(R.string.fragment_about_version_text, BuildConfig.VERSION_NAME));
+
+        setUpGithubButton();
+        setUpFeedbackButton();
+        setUpBugReportButton();
+
+        return mBinding.getRoot();
+    }
+
+
+    private void setUpBugReportButton() {
+
+        mBinding.tvAboutReportBug.setOnClickListener(v -> {
+            String githubURL = getString(R.string.github_repository_url) + "/issues";
+            Uri uri = Uri.parse(githubURL);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            getActivity().startActivity(intent);
+        });
+    }
+
+
+    private void setUpFeedbackButton() {
+
+        mBinding.tvAboutFeedback.setOnClickListener(v -> {
+            Uri uri = Uri.fromParts(
+                    "mailto",
+                    "Alex <" + getString(R.string.support_email) + ">",
+                    null
+            );
+            Intent intent = new Intent(Intent.ACTION_SENDTO, uri)
+                    .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_email_subject));
+            getActivity().startActivity(intent);
+        });
+    }
+
+
+    private void setUpGithubButton() {
+
+        mBinding.tvAboutGithub.setOnClickListener(v -> {
+            String githubURL = getString(R.string.github_repository_url);
+            Uri uri = Uri.parse(githubURL);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            getActivity().startActivity(intent);
+        });
     }
 }
