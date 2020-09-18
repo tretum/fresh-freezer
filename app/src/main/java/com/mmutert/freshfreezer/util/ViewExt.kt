5b4bca4ed8d80c526aecd2ed8fcff2976fdd07ee
michaelmutert <github@michaelmutert.com>
2020-09-19 00:34:50 +0200

    Add new helper and extension classes

---
+++ app/src/main/java/com/mmutert/freshfreezer/util/ViewExt.kt
@@ -0,0 +1,24 @@
+package com.mmutert.freshfreezer.util
+
+import android.view.View
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import com.google.android.material.snackbar.Snackbar
+import com.mmutert.freshfreezer.Event
+import com.mmutert.freshfreezer.EventObserver
+
+fun View.showSnackbar(snackbarText: String, timeLength: Int) {
+    Snackbar.make(this, snackbarText, timeLength).run {
+        show()
+    }
+}
+
+fun View.setupSnackbar(
+        lifecycleOwner: LifecycleOwner,
+        snackbarEvent: LiveData<Event<Int>>,
+        timeLength: Int
+) {
+    snackbarEvent.observe(lifecycleOwner, EventObserver { event ->
+        showSnackbar(context.getString(event), timeLength)
+    })
+}
